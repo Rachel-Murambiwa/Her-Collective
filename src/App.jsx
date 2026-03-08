6b88dabd4de5a66293eaf39a4450cb551ad9123c
Rachel Murambiwa <rachelmurambiwa88@gmail.com>
2026-03-08 11:29:56 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-serif text-espresso mb-4">
+          HerCollective
+        </h1>
+        <p className="text-lg text-espresso/70 tracking-wide">
+          where girlhood meets style
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
