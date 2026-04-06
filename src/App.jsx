329a37ad439ab395837fca8b911da239209902de
Rachel Murambiwa <rachelmurambiwa88@gmail.com>
2026-04-06 15:51:31 +0000

    added a new category and more products

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,7 @@ export default function App() {
     window.open(`https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`, "_blank");
   }
 
-  const navLinks = ['BAGS', 'FASHION ACCESSORIES','ESSENTIALS','PHONE ACCESSORIES', 'HAIR ACCESSORIES', 'NIGHTWEAR', 'ROOM ESSENTIALS'];
+  const navLinks = ['BAGS', 'FASHION ACCESSORIES','BEAUTY','PHONE ACCESSORIES', 'HAIR ACCESSORIES', 'NIGHTWEAR', 'ROOM ESSENTIALS', "TRAVEL ESSENTIALS"];
 
   return (
     <div className="min-h-screen pb-24 relative bg-oatmilk text-espresso font-sans">
